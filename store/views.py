24c61d71d2916d530d4d22c980185ea323094376
Danny Haro <danny.dhs@gmail.com>
2021-01-26 08:13:11 -0500

    Formulario login

--- store/views.py
+++ store/views.py
@@ -16,3 +16,12 @@ def index(request):
             ]
         },
     )
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+    return render(request, 'users/login.html', {
+        
+    })
