240acae5598c7c3aa4938ec42fbe880044a01079
Danny Haro <danny.dhs@gmail.com>
2021-04-02 08:44:56 -0500

    Links y usuarios autenticados

--- store/views.py
+++ store/views.py
@@ -26,6 +26,9 @@ def index(request):
     )
 
 def login_view(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+    
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
@@ -49,6 +52,9 @@ def logout_view(request):
 
 
 def register(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+
     form = RegisterForm(request.POST or None)
     
     if request.method == 'POST' and form.is_valid():        
