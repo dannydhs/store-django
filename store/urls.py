2760c445fa2704b80faadda7bf435633981976dc
Danny Haro <danny.dhs@gmail.com>
2021-04-11 10:29:52 -0500

    Agregando imagenes

--- store/urls.py
+++ store/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 from . import views
 
 from products.views import ProductListView
@@ -14,3 +17,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('productos/', include('products.urls')) # productos/:id
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
