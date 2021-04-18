334e3dd98df761b4518a77d95a3bc6f43b03ce17
Danny Haro <danny.dhs@gmail.com>
2021-04-18 09:45:22 -0500

    Agregando buscador de productos

--- products/views.py
+++ products/views.py
@@ -24,3 +24,21 @@ class ProductDetailView(DetailView): # buscara mediante id -> pk, lo tomara de l
         context = super().get_context_data(**kwargs)
         print(context)
         return context
+
+class ProductSearchListView(ListView):
+    template_name = 'products/search.html'
+
+    def get_queryset(self):
+        # SELECT * FROM products WHERE title LIKE '%valor%'
+        return Product.objects.filter(title__icontains=self.query())
+
+    def query(self):
+        return self.request.GET.get('q')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['query'] = self.query()
+        context['count'] = context['product_list'].count()
+        print(context)
+
+        return context
