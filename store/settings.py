4d285d2c0cf8858c170ad0edd8a72c237fc507e9
Danny Haro <danny.dhs@gmail.com>
2021-04-25 12:34:09 -0500

    Usuarios: agregando usuario abstracto para sobre escribir auth.user

--- store/settings.py
+++ store/settings.py
@@ -54,6 +54,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'store.urls'
 
+AUTH_USER_MODEL = 'users.User'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
--- users/models.py
+++ users/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+
+# AbstractBaseUser: id, password, last_login '''
+# AbstractUser: username, first_name, last_name, email, password, groups, user_permissions,
+# is_staff, is_active, is_superuser, last_login, date_joined
+class User(AbstractUser):
+    def get_full_name(self):
+        return '{} {}'.format(self.first_name, self.last_name)
+
 
 # Proxy model
 class Customer(User): # Para agregar funcionalidad a User
