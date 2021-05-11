9275c68a4793056c422d1f01fafba2d7572b988d
Danny Haro <danny.dhs@gmail.com>
2021-05-11 07:13:21 -0500

    Refactorizando: creando funcion get_or_create_cart para centralizar el manejos de session

--- carts/views.py
+++ carts/views.py
@@ -1,20 +1,10 @@
 from django.shortcuts import render
 
 from .models import Cart
+from .utils import get_or_create_cart
 
 def cart(request):
-    user = request.user if request.user.is_authenticated else None
-    cart_id = request.session.get('cart_id') # None
-    cart = Cart.objects.filter(cart_id=cart_id).first() # [] -> None
-
-    if cart is None:
-        cart = Cart.objects.create(user=user)
-
-    if user and cart.user is None:
-        cart.user = user
-        cart.save()
-
-    request.session['cart_id'] = cart.cart_id
+    cart = get_or_create_cart(request)
 
     return render(request, 'carts/cart.html', {
         
