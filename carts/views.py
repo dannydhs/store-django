6e9e4dd57d1b7e1b39a051f5ed3bf222a59b546c
Danny Haro <danny.dhs@gmail.com>
2021-05-11 05:48:03 -0500

    Creando carrito de compras

--- carts/views.py
+++ carts/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 
+from .models import Cart
+
 def cart(request):
+    user = request.user if request.user.is_authenticated else None
+    cart_id = request.session.get('cart_id')
+
+    if cart_id:
+        cart = Cart.objects.get(pk=cart_id)
+    else:
+        cart = Cart.objects.create(user=user)
+
+    request.session['cart_id'] = cart.id
+
     return render(request, 'carts/cart.html', {
         
     })
