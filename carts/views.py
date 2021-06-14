e39bdfd4b12eaa6c7d3d564d64aa021ccac2caf2
Danny Haro <danny.dhs@gmail.com>
2021-06-13 19:04:33 -0500

    Actualizar cantidad al agregar el mismo producto con otra cantidad, utilizando clase models.Manager para exterder los metodos del .objects

--- carts/views.py
+++ carts/views.py
@@ -1,8 +1,9 @@
+from django.db.models import query
 from django.shortcuts import redirect, render, get_object_or_404
 
 from products.models import Product
 
-from .models import Cart
+from .models import CartProducts
 from .utils import get_or_create_cart
 
 def cart(request):
@@ -17,11 +18,15 @@ def add(request):
     cart = get_or_create_cart(request)
     # product = Product.objects.get(pk=request.POST.get('product_id')) # Se puede utilizar ambos
     product = get_object_or_404(Product, pk=request.POST.get('product_id'))
-    quantity = request.POST.get('quantity', 1)
+    quantity = int(request.POST.get('quantity', 1))
 
-    cart.products.add(product, through_defaults={
-        'quantity': quantity
-    })
+    # cart.products.add(product, through_defaults={
+    #     'quantity': quantity
+    # })
+
+    cart_product = CartProducts.objects.create_or_update_quantity(cart=cart, 
+                                                                product=product, 
+                                                                quantity=quantity)
 
     return render(request, 'carts/add.html', {
         'product': product
